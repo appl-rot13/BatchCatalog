d2563a6e28af5dc5e3776a8dfe4a10355503ae58
rot <appl.rot13@gmail.com>
2023-11-02 09:44:15 +0900

    Fix bug when path contains spaces

--- DeleteFiles.bat
+++ DeleteFiles.bat
@@ -3,7 +3,7 @@
 set TargetFiles="Thumbs.db"
 
 for /f "tokens=*" %%f in (
-  'dir %~dp0 /a-d /b /s ^| findstr %TargetFiles%'
+  'dir "%~dp0" /a-d /b /s ^| findstr %TargetFiles%'
 ) do (
   echo Deleting "%%f"...
   del /a /f "%%f"
